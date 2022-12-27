eeacc7f2ed7eecd5ec64ce22dcf1d41e5e5f4cba
Diego Salmon <diesalmon1@gmail.com>
2022-12-27 19:18:32 -0300

    primer entrega

---
+++ app-react/src/components/ItemListContainer/index.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+import title from "../title";
+
+export const itemlistcontainer = () =>{
+return (
+    <title greeting='Bienvenido' />
+)
+}
+export default itemlistcontainer
